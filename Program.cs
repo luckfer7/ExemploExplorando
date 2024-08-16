1e6af44fb51f43c9bc3f222a83e509b9b64b95cd
luckfer7 <lucasferreira059@gmail.com>
2024-08-16 16:02:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Lucas";
+p1.Sobrenome = "Camilo"; 
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "Carolina";
+p2.Sobrenome = "Camilo";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
